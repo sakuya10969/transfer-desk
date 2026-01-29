97af7704617da2b84c76b0830eb9ecf7bbd6e995
sakuya10969 <sakuya.f20010618@gmail.com>
2026-01-29 19:21:18 +0900

    feat: constants

---
+++ constants/index.ts
@@ -0,0 +1,7 @@
+export const LEAGUES = [
+  { id: 1, name: "Premier League" },
+  { id: 2, name: "Bundesliga" },
+  { id: 3, name: "La Liga" },
+  { id: 4, name: "Serie A" },
+  { id: 5, name: "Ligue 1" },
+];
