5f01f7d5447251cc58ea4012565a69e3ab6c6564
sakuya10969 <sakuya.f20010618@gmail.com>
2026-02-13 17:05:03 +0900

    feat: relay config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  compiler: {
+    relay: {
+      src: ".",
+      artifactDirectory: "./__generated__",
+      language: "typescript",
+    },
+  },
 };
 
 export default nextConfig;
