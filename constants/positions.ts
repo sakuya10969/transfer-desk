6bcd2cc85be232167d0d7427a24a5dd5013baa57
sakuya10969 <sakuya.f20010618@gmail.com>
2026-02-13 13:48:16 +0900

    feat: constants

---
+++ constants/positions.ts
@@ -0,0 +1,15 @@
+export const POSITIONS = [
+  { value: "GK", label: "GK", category: "Goalkeeper" },
+  { value: "CB", label: "CB", category: "Defender" },
+  { value: "LB", label: "LB", category: "Defender" },
+  { value: "RB", label: "RB", category: "Defender" },
+  { value: "DM", label: "DM", category: "Midfielder" },
+  { value: "CM", label: "CM", category: "Midfielder" },
+  { value: "AM", label: "AM", category: "Midfielder" },
+  { value: "LM", label: "LM", category: "Midfielder" },
+  { value: "RM", label: "RM", category: "Midfielder" },
+  { value: "LWG", label: "LWG", category: "Forward" },
+  { value: "RWG", label: "RWG", category: "Forward" },
+  { value: "CF", label: "CF", category: "Forward" },
+  { value: "ST", label: "ST", category: "Forward" },
+] as const;
