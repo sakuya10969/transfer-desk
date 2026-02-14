9a06fd36419e4f33a6965331b7d00091b3722f13
sakuya10969 <sakuya.f20010618@gmail.com>
2026-02-14 16:20:20 +0900

    feat: constants

--- constants/index.ts
+++ constants/index.ts
@@ -4,4 +4,21 @@ export const LEAGUES = [
   { id: 3, name: "ラ・リーガ" },
   { id: 4, name: "セリエA" },
   { id: 5, name: "リーグ・アン" },
-];
+] as const;
+
+export const POSITIONS = [
+  { value: "GK", label: "GK", category: "Goalkeeper" },
+  { value: "CB", label: "CB", category: "Defender" },
+  { value: "LB", label: "LB", category: "Defender" },
+  { value: "RB", label: "RB", category: "Defender" },
+  { value: "DM", label: "DM", category: "Midfielder" },
+  { value: "CM", label: "CM", category: "Midfielder" },
+  { value: "AM", label: "AM", category: "Midfielder" },
+  { value: "LM", label: "LM", category: "Midfielder" },
+  { value: "RM", label: "RM", category: "Midfielder" },
+  { value: "LWG", label: "LWG", category: "Forward" },
+  { value: "RWG", label: "RWG", category: "Forward" },
+  { value: "CF", label: "CF", category: "Forward" },
+  { value: "ST", label: "ST", category: "Forward" },
+] as const;
+
